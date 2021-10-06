f7594e93e628be4aea3c930a8def59c2d38d8ff9
Krish <krish@rubixsoftsolutions.com>
2021-10-05 22:00:52 -0700

    Fixed `index.js`

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-function majorVer(oldVersion, newVersion, options) {
-    if (options.debug) console.log('Starting check...');
-    if (options.debug) console.log('Error checking \'oldVersion\'...');
-    if (!oldVersion) throw new TypeError(`Expected 'string' but got ${oldVersion} (reading parameter 'oldVersion')`);
-    if (options.debug) console.log('Error checking \'newVersion\'...');
-    if (!newVersion) throw new TypeError(`Expected 'string' but got ${newVersion} (reading parameter 'oldVersion')`);
-  
-    let oldArr = oldVersion.split('.');
-    if (options.debug) console.log('Reading \'oldVersion\'...');
-    let newArr = newVersion.split('.');
-    if (options.debug) console.log('Reading \'newVersion\'...');
-    let major;
-    if (options.debug) console.log('Error checking \'oldVersion\'...');
-    if (isNaN(oldArr[0])) throw new TypeError(`Expected 'number' but got ${oldArr[0]} (reading parameter 'oldVersion')`);
-    if (options.debug) console.log('Error checking \'newVersion\'...');
-    if (isNaN(newArr[0])) throw new TypeError(`Expected 'number' but got ${newArr[0]} (reading parameter 'newVersion')`);
-    if (options.debug) console.log('Starting check...');
-    if (oldArr[0] === newArr[0]) {
-      major = false;
-    } else {
-      major = true;
-    }
-    if (options.debug) console.log('Check was successful ✔');
-    return major;
-}  
+module.exports = {
+    majorVer(oldVersion, newVersion, options) {
+        if (options.debug) console.log('Starting check...');
+        if (options.debug) console.log('Error checking \'oldVersion\'...');
+        if (!oldVersion) throw new TypeError(`Expected 'string' but got ${oldVersion} (reading parameter 'oldVersion')`);
+        if (options.debug) console.log('Error checking \'newVersion\'...');
+        if (!newVersion) throw new TypeError(`Expected 'string' but got ${newVersion} (reading parameter 'oldVersion')`);
+    
+        let oldArr = oldVersion.split('.');
+        if (options.debug) console.log('Reading \'oldVersion\'...');
+        let newArr = newVersion.split('.');
+        if (options.debug) console.log('Reading \'newVersion\'...');
+        let major;
+        if (options.debug) console.log('Error checking \'oldVersion\'...');
+        if (isNaN(oldArr[0])) throw new TypeError(`Expected 'number' but got ${oldArr[0]} (reading parameter 'oldVersion')`);
+        if (options.debug) console.log('Error checking \'newVersion\'...');
+        if (isNaN(newArr[0])) throw new TypeError(`Expected 'number' but got ${newArr[0]} (reading parameter 'newVersion')`);
+        if (options.debug) console.log('Starting check...');
+        if (oldArr[0] === newArr[0]) {
+        major = false;
+        } else {
+        major = true;
+        }
+        if (options.debug) console.log('Check was successful ✔');
+        return major;
+    },
+};
